074b58655effda339c27480afdb7101d7e755788
williamlogan <william.logan@mobilecapitalgroup.com>
2017-12-09 11:08:50 -0600

    Improvements to UX, including styles and base functionality

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,12 @@
         <h3>data</h3>
         <p>All elements in a list. Click one to edit.</p>
         <div class="edit-area">
-          <div class="node" v-for="(node, index) in parsedJSON" @click="selected = node, selectedIndex = index">{{ node.name }}</div>
+          <div class="node" v-for="(node, index) in parsedJSON">
+            <label @click="selected = node, selectedIndex = index">
+              {{ node.name }} <i class="fa fa-edit right"></i>
+            </label>
+            <button class="btn danger right" @click="remove(index)"><i class="fa fa-ban"></i></button>
+          </div>
         </div>
       </div>
       <div>
@@ -37,9 +42,11 @@
     </div>
     <div class="actions">
       <div class="input">Unique Key: <input type="text" v-model="key"></div>
-      <div></div>
       <div>
-        <button @click="save()">Save</button>
+        <p v-if="feedback.msg"><i class="fa fa-warning" style="padding: 10px;"></i> {{ feedback.msg }}</p>
+      </div>
+      <div>
+        <!--<button @click="save()">Save</button>-->
       </div>
     </div>
   </div>
@@ -52,35 +59,63 @@
       return {
         msg: 'hello world',
         json: '',
+        parsedJSON: null,
         key: '',
         selected: null,
-        selectedIndex: 0
+        selectedIndex: 0,
+        feedback: {
+          msg: 'All edit operations rely on a unique identifier for each member of the collection.<br /> Please enter the name of a value in the collection member that is unique to each member, such as an id, or a name.'
+        }
       }
     },
     computed: {
-      parsedJSON () {
+      parseJSON () {
         if (this.json) {
           return JSON.parse(this.json);
         }
       },
       newJson () {
         if (this.selected) {
-          return JSON.stringify(this.selected, null, 1);
+          return JSON.stringify(this.parsedJSON, null, 1);
         }
       }
     },
     methods: {
-      save () {
-        let raw = JSON.parse(this.json)
-        for (var i in raw) {
-          if (raw[i][this.key] === this.selected[this.key]) {
-            for (var s in raw[i]) {
-              raw[i][s] = this.selected[s]
-            }
-            break
+      remove (index) {
+        if (this.key) {
+          if (this.selected && this.selected[this.key] === this.parsedJSON[index][this.key]) {
+            this.selected = null
           }
+          this.parsedJSON.splice(index, 1)
         }
-        this.json = JSON.stringify(raw, null, 1);
+      },
+      save () {
+        if (this.key) {
+          let raw = JSON.parse(this.json)
+          for (var i in raw) {
+            if (raw[i][this.key] === this.selected[this.key]) {
+              for (var s in raw[i]) {
+                raw[i][s] = this.selected[s]
+              }
+              break
+            }
+          }
+          this.json = JSON.stringify(raw, null, 1)
+        } else {
+          alert('Saving the JSON requires a uniqe key to look up. Pick the name of a field that is unique per member.')
+        }
+      }
+    },
+    watch: {
+      key () {
+        if (this.key.length) {
+          this.feedback.msg = ''
+        } else {
+          this.feedback.msg = 'All edit operations rely on a unique identifier for each member of the collection.<br /> Please enter the name of a value in the collection member that is unique to each member, such as an id, or a name.'
+        }
+      },
+      json () {
+        this.parsedJSON = JSON.parse(this.json)
       }
     }
   }
